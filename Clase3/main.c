db9f43c41a216980c9a67f48b8b2d33a557ef8fd
alumno <alumno@LAB00PC00>
2018-08-28 22:08:27 -0300

    save 2

--- Clase3/main.c
+++ Clase3/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 int main()
 {
-    printf("Hello world!\n");
+
     return 0;
+}
+
+int pedirEdad(int edad)
+{
+    int validador;
+    printf("Ingresar edad : \");
+    validador=scanf("%d",&edad);
+
+}
+
+
+int validarEdad(int maximo,int minimo, int edad)
+{
+
+
+
+    if (edad < minimo || edad > maximo)
+    {
+        return -1;
+    }
+    else
+    {
+        return edad;
+    }
+
 }
